3164680035e1370fc124b6249b8d028b4664a112
HariBalan410 <harigamer070727@gmail.com>
2026-08-17 14:23:48 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        new_node = Node(data)
+
+        if self.root is None:
+            self.root = new_node
+            return
+
+        current = self.root
+
+        while True:
+            if data < current.data:
+                if current.left is None:
+                    current.left = new_node
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = new_node
+                    break
+                current = current.right
+
+    def inorder(self, node):
+        if node is not None:
+            self.inorder(node.left)
+            print(node.data, end=" ")
+            self.inorder(node.right)
+
+
+tree = BinaryTree()
+
+n = int(input("Enter number of book titles: "))
+
+for i in range(n):
+    title = input("Enter book title: ")
+    tree.insert(title)
+
+print("\nBook titles in Inorder Traversal:")
+tree.inorder(tree.root)
